36515056aaa23ce12eb5f92fb6ea3bf24fa60120
Allison Rodrigues <allison.ifg@gmail.com>
2020-11-28 10:50:12 -0300

    Parte JS
    Começando a parte em JS, referenciando botão, lista e a entrada de dados.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Referenciar input
+
+let input = document.querySelector('input[name=tarefa')
+
+//referenciar botão
+
+let botao = document.querySelector('#botao')
+
+//referenciar lista
+
+let lista = document.querySelector('#lista')
+
+//representando a lista de tarefas
+
+let tarefas = [
+  'Jogar',
+  'Estudar',
+  'Comer',
+  'Ver TV',
+  'Exercitar'
+]
+
+/*
+    <li class="list-group-item list-group-item-action">Jogar</li>
+    <li class="list-group-item list-group-item-action">Estudar</li>
+    <li class="list-group-item list-group-item-action">Comer</li>
+    <li class="list-group-item list-group-item-action">Ver TV</li>
+    <li class="list-group-item list-group-item-action">Exercitar</li>
+*/
